0c02ca148ce358f0ca421455a195eaeeba090962
NoNameA 774 <nonamea774@gmail.com>
2015-06-23 02:48:18 +0900

    use function && parent commit pointer

--- make-commit.rb
+++ make-commit.rb
@@ -6,6 +6,15 @@ AUTHOR = "NoNameA 774 <nonamea774@gmail.com>"
 TIMEZONE = "+0900"
 date = `date +%s`.chomp
 
+def saveFile(str)
+  sha1 = Digest::SHA1.hexdigest(str)
+  content = Zlib::Deflate.deflate(str)
+  path = '.git/objects/' + sha1[0,2] + '/' + sha1[2,38]
+  FileUtils.mkdir_p(File.dirname(path))
+  File.open(path, 'w') { |f| f.write content }
+  return sha1
+end
+
 args = ARGV
 commitMsg = args.shift
 
@@ -28,18 +37,14 @@ args.each do |file|
 end
 
 tree = "tree #{treeTmp.length}\0" + treeTmp
-treeSha1 = Digest::SHA1.hexdigest(tree)
-content = Zlib::Deflate.deflate(tree)
-treePath = '.git/objects/' + treeSha1[0,2] + '/' + treeSha1[2,38]
-FileUtils.mkdir_p(File.dirname(treePath))
-File.open(treePath, 'w') { |f| f.write content }
+treeSha1 = saveFile(tree)
 
-commitTmp = "tree #{treeSha1}\nauthor #{AUTHOR} #{date} #{TIMEZONE}\ncommitter #{AUTHOR} #{date} #{TIMEZONE}\n\n" + commitMsg + "\n"
+parent = ""
+if File.exist?(".git/refs/heads/master")
+  parent = "parent " + File.open(".git/refs/heads/master").read
+end
+commitTmp = "tree #{treeSha1}\n#{parent}author #{AUTHOR} #{date} #{TIMEZONE}\ncommitter #{AUTHOR} #{date} #{TIMEZONE}\n\n" + commitMsg + "\n"
 commit = "commit #{commitTmp.length}\0" + commitTmp
-commitSha1 = Digest::SHA1.hexdigest(commit)
-content = Zlib::Deflate.deflate(commit)
-path = '.git/objects/' + commitSha1[0,2] + '/' + commitSha1[2,38]
-FileUtils.mkdir_p(File.dirname(path))
-File.open(path, 'w') { |f| f.write content }
+commitSha1 = saveFile(commit)
 
 `echo #{commitSha1} > .git/refs/heads/master`
